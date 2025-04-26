412c57d7636393d0f7f58f60ce83a2c250df9fa0
Joe Bradley <joe@districtapps.com>
2025-04-27 01:26:05 +0300

    feat: Hard-coded video url

--- backend/core/config.py
+++ backend/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     TEST_DATABASE_URL: Optional[str] = None
     
     # Video Settings
-    PARLIAMENT_TV_URL: str = "https://www.parliamentlive.tv/Event/Index"
+    PARLIAMENT_TV_URL: str = "https://www.parliamentlive.tv/Event/Index/56fac7d5-b3be-4f69-94f3-e68ffc46c9c1"
     TEMP_STORAGE_PATH: str = "/app/data/temp"
     MEDIA_STORAGE_PATH: str = "/app/data/media"
     MAX_CLIP_DURATION_MINUTES: int = 30
