3b82f5f12e336c9b94f36b5299fbf906030bb0e1
Joe Bradley <joe@veedoo.io>
2025-05-25 22:34:08 +0300

    feat: Get rid of parliament tv and videos in favor of file gallery

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -38,15 +38,9 @@ const Navbar: React.FC = () => {
               </Link>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <Link href="/videos">
-                <span className="border-transparent text-gray-500 dark:text-gray-300 hover:border-primary hover:text-primary dark:hover:text-blue-400 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
-                  Video Gallery
-                </span>
-              </Link>
-              
               <Link href="/files">
                 <span className="border-transparent text-gray-500 dark:text-gray-300 hover:border-primary hover:text-primary dark:hover:text-blue-400 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
-                  File Gallery
+                  Media Library
                 </span>
               </Link>
 
@@ -184,15 +178,9 @@ const Navbar: React.FC = () => {
             
 
             
-            <Link href="/videos">
-              <span className="border-transparent text-gray-500 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-100 block pl-3 pr-4 py-2 border-l-4 text-base font-medium cursor-pointer">
-                Video Gallery
-              </span>
-            </Link>
-            
             <Link href="/files">
               <span className="border-transparent text-gray-500 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-100 block pl-3 pr-4 py-2 border-l-4 text-base font-medium cursor-pointer">
-                File Gallery
+                Media Library
               </span>
             </Link>
             
