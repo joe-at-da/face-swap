87c1c8bb4e6868b41138d130401ef7f51ca3680f
Joe Bradley <joe@veedoo.io>
2025-05-27 13:22:04 +0300

    feat: Updates for side-menu

--- frontend/components/layout/DarkLayout.tsx
+++ frontend/components/layout/DarkLayout.tsx
@@ -36,16 +36,6 @@ const DarkLayout: React.FC<DarkLayoutProps> = ({
       name: 'Media',
       items: [
         {
-          name: 'Videos',
-          href: '/videos',
-          icon: 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
-        },
-        {
-          name: 'Video Clips',
-          href: '/clips',
-          icon: 'M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z',
-        },
-        {
           name: 'File Gallery',
           href: '/files',
           icon: 'M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z',
@@ -64,11 +54,6 @@ const DarkLayout: React.FC<DarkLayoutProps> = ({
           name: 'Transcriptions',
           href: '/transcriptions',
           icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
-        },
-        {
-          name: 'Parliament TV',
-          href: '/parliament-tv/videos',
-          icon: 'M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z',
         },
       ],
     },
